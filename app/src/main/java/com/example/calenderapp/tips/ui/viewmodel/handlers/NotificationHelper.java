4f0fef2aa7ec4a05b3d66c58bae063f359f411ed
aialmohamed <ahmaedibrahim311@gmail.com>
2024-01-15 13:50:54 +0100

    Edit: now the Notification are clickable

--- app/src/main/java/com/example/calenderapp/tips/ui/viewmodel/handlers/NotificationHelper.java
+++ app/src/main/java/com/example/calenderapp/tips/ui/viewmodel/handlers/NotificationHelper.java
@@ -2,16 +2,19 @@ package com.example.calenderapp.tips.ui.viewmodel.handlers;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
 
+import com.example.calenderapp.Notification.AllTipsView;
 import com.example.calenderapp.tips.model.HandlerItems.TipNotification;
 import com.example.calenderapp.tips.model.HandlerItems.TipNotificationChannel;
 
 public class NotificationHelper {
     private TipNotification tipNotification;
-
+    private Intent NotificationIntent;
     private TipNotificationChannel tipNotificationChannel;
 
     private Context tipcontext;
@@ -22,14 +25,17 @@ public class NotificationHelper {
         this.tipNotification = tipNotification;
         this.tipNotificationChannel = tipNotificationChannel;
         this.tipcontext = tipcontext;
+        NotificationIntent = new Intent(tipcontext, AllTipsView.class);
     }
 
     private Notification CreateNotification()
     {
+        PendingIntent notificationClickIntent = PendingIntent.getActivity(tipcontext,1,NotificationIntent,PendingIntent.FLAG_IMMUTABLE);
         Notification notification = new NotificationCompat.Builder(tipcontext,tipNotificationChannel.getChannelId())
                 .setSmallIcon(tipNotification.getTipNotificationIcon())
                 .setContentTitle(tipNotification.getNotificationTitle())
                 .setContentText(tipNotification.getNotificationText())
+                .setContentIntent(notificationClickIntent)
                 .setChannelId(tipNotificationChannel.getChannelId())
                 .setPriority(tipNotification.getTipNotificationPriority())
                 .build();
